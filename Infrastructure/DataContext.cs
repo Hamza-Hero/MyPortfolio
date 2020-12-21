3dd9a9feee9209ccc5638ac6ec6e5e25e9ee04a1
hamza.khabbaz <hamza.khabbaz@softwarehelden.com>
2020-12-21 18:14:10 +0100

    DataContext

---
+++ Infrastructure/DataContext.cs
@@ -0,0 +1,39 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // ID wird automatisch erstellt
+            modelBuilder.Entity<Owner>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+            modelBuilder.Entity<PortfolioItem>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+
+            modelBuilder.Entity<Owner>().HasData(
+                new Owner()
+                {
+                    Id = Guid.NewGuid(),
+                    Avatar = "Hamza.png",
+                    FullName = "Hamza Khabbaz",
+                    Profil = "Software Developer / Photograph"
+                }
+            );
+        }
+
+        public DbSet<Owner> Owner { get; set; }
+        public DbSet<PortfolioItem> PortfolioItems { get; set; }
+    }
+}
