050b07a381feba10041756173173575133dad301
hamza.khabbaz <hamza.khabbaz@softwarehelden.com>
2021-02-22 12:29:36 +0100

    Fields Validation

--- Web/Controllers/PortfolioItemsController.cs
+++ Web/Controllers/PortfolioItemsController.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using Core.Entities;
-using Infrastructure;
-using Web.ViewModels;
-using System.IO;
-using Microsoft.AspNetCore.Hosting;
 using Core.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web.ViewModels;
 
 namespace Web.Controllers
 {
@@ -55,34 +52,31 @@ namespace Web.Controllers
         }
 
         // POST: PortfolioItems/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(PortfolioViewModel model)
         {
-            if (ModelState.IsValid)
+            if (model.File != null)
             {
-                if (model.File != null)
-                {
-                    string uploads = Path.Combine(_hosting.WebRootPath, @"img\portfolio");
-                    string fullPath = Path.Combine(uploads, model.File.FileName);
-                    model.File.CopyTo(new FileStream(fullPath, FileMode.Create));
-                }
-
-                PortfolioItem portfolioItem = new PortfolioItem
-                {
-                    ProjectName = model.ProjectName,
-                    Description = model.Description,
-                    ImageUrl = model.File.FileName
-                };
-
-                _portfolio.Entity.Insert(portfolioItem);
-                _portfolio.Save();
-                return RedirectToAction(nameof(Index));
+                string uploads = Path.Combine(_hosting.WebRootPath, @"img\portfolio");
+                string fullPath = Path.Combine(uploads, model.File.FileName);
+                model.File.CopyTo(new FileStream(fullPath, FileMode.Create));
             }
 
-            return View(model);
+            PortfolioItem portfolioItem = new PortfolioItem
+            {
+                ProjectName = model.ProjectName,
+                Description = model.Description,
+                ImageUrl = model.File.FileName
+            };
+
+            _portfolio.Entity.Insert(portfolioItem);
+            _portfolio.Save();
+            return RedirectToAction(nameof(Index));
+
+            //return View(model);
         }
 
         // GET: PortfolioItems/Edit/5
@@ -111,7 +105,7 @@ namespace Web.Controllers
         }
 
         // POST: PortfolioItems/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -122,42 +116,40 @@ namespace Web.Controllers
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+            try
             {
-                try
+                if (model.File != null)
                 {
-                    if (model.File != null)
-                    {
-                        string uploads = Path.Combine(_hosting.WebRootPath, @"img\portfolio");
-                        string fullPath = Path.Combine(uploads, model.File.FileName);
-                        model.File.CopyTo(new FileStream(fullPath, FileMode.Create));
-                    }
-
-                    PortfolioItem portfolioItem = new PortfolioItem
-                    {
-                        Id = model.Id,
-                        ProjectName = model.ProjectName,
-                        Description = model.Description,
-                        ImageUrl = model.File.FileName
-                    };
-
-                    _portfolio.Entity.Update(portfolioItem);
-                    _portfolio.Save();
+                    string uploads = Path.Combine(_hosting.WebRootPath, @"img\portfolio");
+                    string fullPath = Path.Combine(uploads, model.File.FileName);
+                    model.File.CopyTo(new FileStream(fullPath, FileMode.Create));
                 }
-                catch (DbUpdateConcurrencyException)
+
+                PortfolioItem portfolioItem = new PortfolioItem
                 {
-                    if (!PortfolioItemExists(model.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                    Id = model.Id,
+                    ProjectName = model.ProjectName,
+                    Description = model.Description,
+                    ImageUrl = model.File.FileName
+                };
+
+                _portfolio.Entity.Update(portfolioItem);
+                _portfolio.Save();
             }
-            return View(model);
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PortfolioItemExists(model.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+
+            //return View(model);
         }
 
         // GET: PortfolioItems/Delete/5
--- Web/ViewModels/PortfolioViewModel.cs
+++ Web/ViewModels/PortfolioViewModel.cs
@@ -1,17 +1,22 @@
-﻿using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
 
 namespace Web.ViewModels
 {
     public class PortfolioViewModel
     {
         public Guid Id { get; set; }
+
+        [Required]
         public string ProjectName { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
         public string ImageUrl { get; set; }
+
         public IFormFile File { get; set; }
     }
 }
